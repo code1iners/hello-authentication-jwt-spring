5028f79f6abed83c94c26069674094fa267af815
Codeliner <codeliner96@gmail.com>
2021-07-13 20:44:26 +0900

    Created a new project.

---
+++ src/test/java/hello/condeliner/jwt/JwtApplicationTests.java
@@ -0,0 +1,13 @@
+package hello.condeliner.jwt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
